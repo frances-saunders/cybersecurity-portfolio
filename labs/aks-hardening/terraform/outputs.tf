fe6e4b8d1623001c9b7210d6e3915b3c7f9f4fad
Frances Saunders <92679675+frances-saunders@users.noreply.github.com>
2025-08-17 11:55:32 -0400

    Update outputs.tf

--- labs/aks-hardening/terraform/outputs.tf
+++ labs/aks-hardening/terraform/outputs.tf
@@ -1,17 +1,43 @@
-// Terraform outputs for AKS Hardening Lab
+// -----------------------------
+// AKS Cluster Outputs
+// -----------------------------
+
+output "resource_group_name" {
+  description = "Resource group containing the AKS cluster"
+  value       = azurerm_resource_group.rg.name
+}
 
 output "aks_cluster_name" {
-  description = "The name of the AKS cluster"
+  description = "Name of the deployed AKS cluster"
   value       = azurerm_kubernetes_cluster.aks.name
 }
 
+output "aks_cluster_id" {
+  description = "Resource ID of the AKS cluster"
+  value       = azurerm_kubernetes_cluster.aks.id
+}
+
+output "aks_fqdn" {
+  description = "FQDN of the AKS cluster API server"
+  value       = azurerm_kubernetes_cluster.aks.fqdn
+}
+
 output "aks_kube_config" {
-  description = "Kube config for the AKS cluster"
+  description = "Kube config for connecting to the cluster"
   value       = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive   = true
 }
 
-output "policy_assignment_id" {
-  description = "The ID of the AKS Security Baseline assignment"
+// -----------------------------
+// Policy Outputs
+// -----------------------------
+
+output "aks_security_baseline_initiative_id" {
+  description = "Resource ID of the AKS Security Baseline Initiative"
+  value       = azurerm_policy_set_definition.aks_security_baseline.id
+}
+
+output "aks_security_baseline_assignment_id" {
+  description = "Resource ID of the AKS Security Baseline Assignment"
   value       = azurerm_policy_assignment.aks_security_baseline.id
 }
