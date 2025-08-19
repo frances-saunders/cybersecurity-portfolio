895b7f10a32c926c37218fcb750c746c1afcf06f
Frances Saunders <92679675+frances-saunders@users.noreply.github.com>
2025-08-18 20:24:01 -0400

    Update main.tf

--- labs/aks-hardening/terraform/main.tf
+++ labs/aks-hardening/terraform/main.tf
@@ -7,6 +7,9 @@ provider "azurerm" {
   features {}
 }
 
+# Data block to get current tenant
+data "azurerm_client_config" "current" {}
+
 # Resource Group
 resource "azurerm_resource_group" "aks_rg" {
   name     = var.resource_group_name
@@ -24,10 +27,25 @@ resource "azurerm_key_vault" "aks_kv" {
   purge_protection_enabled    = true
 }
 
-# Store AKS Admin Password in Key Vault
+# Store AKS Admin Password in Key Vault (if required)
 resource "azurerm_key_vault_secret" "aks_admin_password" {
   name         = "aks-admin-password"
   value        = var.aks_admin_password   # Sensitive variable, no default
+  key_vault_id = azurerm_key_vault.aks_kv.id
+}
+
+# Allow AKS identity to access Key Vault
+resource "azurerm_key_vault_access_policy" "aks_policy" {
+  key_vault_id = azurerm_key_vault.aks_kv.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+
+  secret_permissions = ["Get", "List"]
+}
+
+# Retrieve password securely from Key Vault (if required)
+data "azurerm_key_vault_secret" "aks_admin_password" {
+  name         = azurerm_key_vault_secret.aks_admin_password.name
   key_vault_id = azurerm_key_vault.aks_kv.id
 }
 
@@ -50,15 +68,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   linux_profile {
     admin_username = var.admin_username
-
     ssh_key {
       key_data = var.ssh_public_key
     }
+    # Uncomment only if password login is required
+    # password = data.azurerm_key_vault_secret.aks_admin_password.value
   }
-
-  # No plaintext password — pulled from Key Vault instead
-  # Password reference for admin login is stored securely
 }
-
-# Data block to get current tenant
-data "azurerm_client_config" "current" {}
